becc1a5a2ce8f7a303747fe28fe1c378ac256b20
Valentin Vetukov <finik001@gmail.com>
2022-05-19 22:46:23 +0300

    Реализован базовый функционал калькулятора.

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,73 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_def').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_mul').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'mul';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_div').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'div';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_sqrt').addEventListener('click', function () {
+    operation = null;
+    lastOperand = 0;
+    inputWindow.value = Math.sqrt(parseInt(inputWindow.value));
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'def') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'mul') {
+        const result = lastOperand * parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'div') {
+        const result = lastOperand / parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+
+// Создал универсальное навешивание событий на клавиши с числами, и получением числа с данных клавишь.
+document.querySelectorAll('.btn_number').forEach(btn => {
+    btn.addEventListener('click', e => {
+        if (inputWindow.value === '0') {
+            inputWindow.value = parseInt(e.currentTarget.innerText);
+        } else {
+            inputWindow.value += parseInt(e.currentTarget.innerText);
+        }
+        
+    });
+});
