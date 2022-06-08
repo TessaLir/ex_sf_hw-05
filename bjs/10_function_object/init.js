1f67733d8d38666c8870feae67e04d621e211249
Valentin Vetukov <finik001@gmail.com>
2022-06-08 16:03:27 +0300

    add buttons refresh and clear

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,24 +1,77 @@
 
 window.onload = function()
 {
-    const initPerson = personGenerator.getPerson();
-
     // В методе был указо получение элементов через getElementById заменил на querySelector
+    const firstNameOutput = document.querySelector('#firstNameOutput'),
+          surnameOutput = document.querySelector('#surnameOutput'),
+          patronymicOutput = document.querySelector('#patronymicOutput'),
+          birthYearOutput = document.querySelector('#birthYearOutput'),
+          birthMounthOutput = document.querySelector('#birthMounthOutput'),
+          birthDayOutput = document.querySelector('#birthDayOutput'),
+          genderOutput = document.querySelector('#genderOutput'),
+          professionOutput = document.querySelector('#professionOutput'),
+          avatar = document.querySelector('.avatar')
 
-    // Добавление ФИО на страницу
-    document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
-    document.querySelector('#surnameOutput').innerText = initPerson.surname;
-    document.querySelector('#patronymicOutput').innerText = initPerson.patronymic;
+    let initPerson;
 
-    // Добавление Даты рождения на страницу
-    document.querySelector('#birthYearOutput').innerText = initPerson.birthYear;
-    document.querySelector('#birthMounthOutput').innerText = initPerson.birthMonth.name;
-    document.querySelector('#birthDayOutput').innerText = initPerson.birthDay;
+    generateRandomPerson();
 
-    // Добавление прочей информации на страницу
-    document.querySelector('#genderOutput').innerText = initPerson.gender;
-    document.querySelector('#professionOutput').innerText = initPerson.profession;
-    document.querySelector('.avatar').innerText = initPerson.embodies;
+    // На кнопку напешан бработчик событий с действием геренации случайного пользователя.
+    document.querySelector('#button-generation').addEventListener('click', () => {
+        clearAll();                 // Для начала очистим данные.
+        generateRandomPerson();     // Собирем новые данные.
+    });
+
+    // На кнопку навешен обработчик событий с действием очистки данных
+    document.querySelector('#button-cleaner').addEventListener('click', () => {
+        clearAll();
+    });
+
+
+    // Метод запуска генерации рандомного пользователя.
+    function generateRandomPerson() {
+        // Создадим нового пользователя.
+        initPerson = personGenerator.getPerson();
+
+        // Добавление ФИО на страницу
+        firstNameOutput.innerText = initPerson.firstName;
+        surnameOutput.innerText = initPerson.surname;
+        patronymicOutput.innerText = initPerson.patronymic;
+
+        // Добавление Даты рождения на страницу
+        birthYearOutput.innerText = initPerson.birthYear;
+        birthMounthOutput.innerText = initPerson.birthMonth.name;
+        birthDayOutput.innerText = initPerson.birthDay;
+
+        // Добавление прочей информации на страницу
+        genderOutput.innerText = initPerson.gender;
+        professionOutput.innerText = initPerson.profession;
+        avatar.innerText = initPerson.embodies;
+    }
+
+    // Метод для очистки данных.
+    function clearAll () {
+        // Проверка есть ли данные, если их нету. то и нечего очищать.
+        if (initPerson) {
+            // Надо удалить старого пользователя. для этого присваиваем переменной NULL
+            initPerson = null;
+
+            // Почистим форму.
+            firstNameOutput.innerText = 'имя';
+            surnameOutput.innerText = 'фамилия';
+            patronymicOutput.innerText = 'отчество';
+            birthYearOutput.innerText = 'год';
+            birthMounthOutput.innerText = 'месяц';
+            birthDayOutput.innerText = 'день';
+            genderOutput.innerText = 'пол';
+            professionOutput.innerText = 'профессия';
+            avatar.innerText = 'х';
+
+            //TODO По хорошему надо отключить кномпку.
+        }
+
+        //TODO Тут можно добавить еще события ELSE вывести какое - либо сообщение пользователю.
+    }
 
 };
 
